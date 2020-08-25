21f33bdc7490fdaa2e7f5b04df201e3971790147
Filipe Klinger <filipeklinger@gmail.com>
2020-08-25 12:05:38 -0300

    criando obj censo

--- Program.js
+++ Program.js
@@ -12,3 +12,37 @@ const getCsvArchive = (calback)=>{
         success: (data)=> calback(data)
     })
 }
+
+class censo {
+    Local = "";
+    Populacao = 0;
+
+    constructor(localName,populacao) {
+        this.setLocal(localName)
+        this.setPopulacao(populacao)
+    }
+
+    setLocal(local){
+        this.Local = local? local : "Não informado";
+    }
+
+    setPopulacao(populacao){
+        try {
+            populacao = parseInt(populacao)
+            this.Populacao = isNaN(populacao)? 0 : populacao
+        } catch (e) {
+            this.Populacao = 0
+        }
+    }
+
+    getLocal() {
+        return this.Local
+    }
+    getPopulacao() {
+        return this.Populacao
+    }
+
+    getPopulacaoDobro(){
+        return this.populacao*2
+    }
+}
